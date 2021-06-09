3ba34399cf730a065de1bfdf74ae00a90b62640b
Deepesh1502 <63232108+Deepesh1502@users.noreply.github.com>
2021-06-09 20:36:58 +0530

    Create NestedIf.c

---
+++ Statements/NestedIf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+  int a;
+  printf("Enter a number");
+  scanf("%d", 7a);
+    if(a%2==0) {
+      if(a%11==0) {
+        printf("Number is mutiple of 11");
+      }
+      else{
+        printf("Number is even");
+      }
+    }
+  else{
+    printf("Number is odd");
+  }
+}
