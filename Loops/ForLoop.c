2bcac89bb1ccb54622bcee59b76603b16fb076bf
Deepesh1502 <63232108+Deepesh1502@users.noreply.github.com>
2021-06-09 16:32:20 +0530

    Create ForLoop.c

---
+++ Loops/ForLoop.c
@@ -0,0 +1,13 @@
+/*The for loop has three parts
+First is initialization will intialize a value to variable i
+Second is condition this is used to break the for loop
+Third is Increment/Decrement which increasse/decrease the value of i by 1 
+*/
+#include <stdio.h>
+int main()
+{
+  printf("For Loop:");
+  int i; //the int keyword will assign a value to variable i
+  for(i=1; i<10; i++);
+  printf(i);
+  )
