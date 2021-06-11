710973863b04fd8e3817eaf7efff6a5d82d80e6d
Deepesh1502 <63232108+Deepesh1502@users.noreply.github.com>
2021-06-12 00:55:00 +0530

    Update WhileLoop.c

--- Loops/WhileLoop.c
+++ Loops/WhileLoop.c
@@ -1,11 +1,14 @@
-//
+//A while loop is a type of loop which keeps on looping until condition met
 #include <stdio.h>
 int main()
 {
-  int i;
-   scanf("%d%d", &i);
-    while (i<=10) {
-    printf("%d\n", i);
+  int i, n;
+  printf("Enter the value to start loop:");
+   scanf(" %d", &i); //This part will take value to start the loop
+  printf("enter the value to end loop:");
+   scanf(" %d", &n); //this part will take value to end the loop
+   while (i<=n) {
+  printf("%d\n", i);
       i++;
     }
 }
